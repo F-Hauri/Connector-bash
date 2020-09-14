f1062db75c2afc5e29f4eafd2c84009fc3bef7c0
F-Hauri <github@f-hauri.ch>
2020-09-14 08:57:46 +0200

    - Replacement of FIFO by the use of `coproc` and `exec {VARNAME}<> <(:)` - Use of `local -n` to avoid eval

--- README.md
+++ README.md
@@ -11,38 +11,37 @@ Introduction:
 In order to avoid multiple forks to some regular tools like `date`, `bc`
 and any *line-by-line* converter, based on
 
-    mkfifo /tmp/myFifoForBc
-    exec 5> >(bc -l >/tmp/myFifoForBc)
-    exec 6</tmp/myFifoForBc
-    rm /tmp/myFifoForBc
+    coproc bc -l
 
 Then
 
-    echo "3*4" >&5
-    read -u 6 foo
+    echo "3*4" >&${COPROC[1]}
+    read -u $COPROC foo
     echo $foo
     12
 
-    echo >&5 "pi=4*a(1)"
-    echo >&5 "2*pi*12"
-    read -u 6 foo
+    echo >&${COPROC[1]} "pi=4*a(1)"
+    echo >&${COPROC[1]} "2*pi*12"
+    read -u $COPROC foo
     echo $foo
     75.39822368615503772256
 
 There is a function for building I/O connectors:
 
-    . shell_connector.sh
+    . shell_connector.bash
     newConnector /usr/bin/bc "-l" 1 1
     myBc '4*a(1)' PI
     declare -p PI
     declare -- PI="3.14159265358979323844"
 
-And for tools able to answer *0* or *many* lines, like *SQL Clients*, there is another function:
+And for tools able to answer *0* or *many* lines, like *SQL Clients*,
+there is another function:
 
     ostty=$(stty -g) && stty -echo
     newSqlConnector /usr/bin/mysql "-h hostOrIp -p'$(head -n1)' -B database"
     stty $ostty
-    myMysql answer 'SELECT * FROM mytable;'
-    declare -p answer_h answer
- 
-where both variables are *arrays*, first one `answer_h` will hole header of answer and `answer` is the complete answer.
+    mySqlReq answer 'SELECT * FROM mytable;'
+    declare -p answer answer_h answer_e
+
+where all variables are *arrays*, first one `answer` will contain all rows,
+`answer_h` will hold header and `answer_e` error messages if any..
--- shell_connector.bash
+++ shell_connector.bash
@@ -1,148 +1,102 @@
 #!/bin/bash
 
-# Connector fifos directory
-read TMPDIR < <(mktemp -d /dev/shm/bc_shell_XXXXXXX)
-
-fd=1
-# find next free fd
-nextFd() {
-    while [ -e /dev/fd/$fd ];do ((fd++)) ;done
-}
+mkBound() {
+    # Building some uniq 30 random char string from 12 $RANDOM values:
+    # RANDOM is 15 bits. 15 x 2 = 30 bits -> 5 x 6 bits char
+    local -n result=${1:-bound}
+    local _Bash64_refstr _out= _l _i _num
+    printf -v _Bash64_refstr "%s" {0..9} {a..z} {A..Z} @ _ 0
+    for ((_l=6;_num=(RANDOM<<15|RANDOM),_l--;));do
+        for ((_i=0;_i<30;_i+=6));do
+            _out+=${_Bash64_refstr:(_num>>_i)&63:1}
+        done
+    done
+    printf -v result -- "--%s-%s-%s-%s-%s-%s-" ${_out:0:7} \
+           ${_out:7:4} ${_out:11:4} ${_out:15:4} \
+           ${_out:19:4} ${_out:23};
+}    
 
 # instanciator for myXxx() function associated to a started and long-running
 # instance of the requested command and arguments and initial input data
-# (the command will be associated to two descriptors IN and OUT, local to
+# (the command will be associated to two descriptors XXIN and XXOUT, local to
 #  the instanciated function)
 newConnector() {
     local command="$1" cmd=${1##*/} args="$2" check="$3" verif="$4"
-    local initfile cinfd=${cmd^^}IN coutfd=${cmd^^}OUT FIFO=$TMPDIR/$cmd input
     shift 4
-    mkfifo $FIFO
-    nextFd
-    eval "exec $fd> >(LANG=C exec stdbuf -o0 $command $args >$FIFO 2>&1) ; $cinfd=$fd;"
-    nextFd
-    eval "exec $fd<$FIFO;$coutfd=$fd;"
-    
+    local initfile input
+    local -n cinfd=${cmd^^}IN coutfd=${cmd^^}OUT
+
+    coproc stdbuf -o0 $command $args 2>&1
+    cinfd=${COPROC[1]} coutfd=$COPROC 
+
     for initfile ;do
-	cat >&${!cinfd} $initfile
+	cat >&${cinfd} $initfile
     done
 
     source <(echo "my${cmd^}() {
-		local in;
- 		echo >&${!cinfd} \"\$1\" &&
- 		read -u ${!coutfd} \${2:-in};
- 		((\$#==2)) || echo \$in;
+		local -n result=\${2:-${cmd}Out}
+		echo >&\${${cmd^^}IN} \"\$1\" &&
+		read -u \${${cmd^^}OUT} -t 3 result
+		((\$#>1)) || echo \$result
 		}"
-       )
+	)
 
     my${cmd^} $check input
     [ "$input" = "$verif" ] ||
 	printf >&2 "WARNING: Don't match! '%s' <> '%s'.\n" "$verif" "$input"
-    rm $FIFO
 }
+
 
 # SQL Connector
 # Stronger, because output length is not fixed, could by empty.
-declare bound SQLIN SQLOUT SQLERR lastsqlread
+# this work with "sqlite", but also with "mysql", "mariadb" or "postgresql"
+declare bound sqlreqbound SQLIN SQLOUT SQLERR lastsqlread
 newSqlConnector() {
-    local command="$1" cmd=${1##*/} args="$2" check="$3" verif="$4"
-    # this work with "sqlite", but also with "mysql", "mariadb" or "postgresql"
-
-    # First building some uniq bound string
-    local _Bash64_refstr _out=-- _l _i _num
-    printf -v _Bash64_refstr "%s" {0..9} {a..z} {A..Z} @ _ 0
-    for ((_l=6;_l--;));do
-	_num=$((RANDOM<<15|RANDOM))
-	for ((_i=0;_i<30;_i+=6));do
-	    _out+=${_Bash64_refstr:(_num>>_i)&63:1}
-	done
-    done
-    printf -v bound "%s-%s-%s-%s-%s" ${_out:0:8} \
-	   ${_out:8:4} ${_out:12:4} ${_out:16:4} ${_out:20};
-    # Initiate long running sqlite with 2 output feeds.
-    FIFOUT=$TMPDIR/sqlout
-    FIFERR=$TMPDIR/sqlerr
-    mkfifo $FIFOUT
-    mkfifo $FIFERR
-    nextFd
-    eval "exec $fd> >(exec stdbuf -o0 $command $args >$FIFOUT 2>$FIFERR)"
-    SQLIN=$fd
-    nextFd
-    eval "exec $fd<$FIFOUT;"
-    SQLOUT=$fd
-    nextFd
-    eval "exec $fd<$FIFERR;"
-    SQLERR=$fd
-    rm $FIFOUT $FIFERR
+    local command="$1" cmd=${1##*/} args check="$3" verif="$4" COPROC
+    IFS=' ' read -a args <<<"$2"
+    local -n _sqlin=SQLIN _sqlout=SQLOUT
+    mkBound bound
+    case $cmd in
+        psql ) sqlreqbound='EXTRACT(\047EPOCH\047 FROM now())' ;;
+        mysql|mariadb ) sqlreqbound='UNIX_TIMESTAMP()' ;;
+        sqlite* ) sqlreqbound='STRFTIME(\047%%s\047,DATETIME(\047now\047))' ;;
+        * ) echo >&2 "WARNING '$cmd' not known as SQL client";;
+    esac
+    
+    exec {SQLERR}<> <(: p)
+    coproc stdbuf -o0 $command "${args[@]}" 2>&$SQLERR
+    _sqlin=${COPROC[1]} _sqlout=$COPROC 
 }
-
-# newSqlConnector /usr/bin/sqlite3 "-separator $'\t' -header /dev/shm/test.sqlite"
-# newSqlConnector /usr/bin/psql    "-Anh hostname -F $'\t' --pset=footer=off user"
-# newSqlConnector /usr/bin/mysql   "-h hostname -B -p database"
-
-mySqlite() {
-    # return nothing but errors via stderr (comment `echo >&2` line and
-    # uncomment previous line with `eval ${result}_e` for changing this
-    # behaviour) this set two (or tree if error) variables: `$1` containing
-    # sql answer and `${1}_h` containing header fields (and ${1}_e for
-    # errors if uncommented).
-    local result=$1 line head=""
+mySqlReq() {
+    # return nothing but set two (or tree if error) variables: `$1`, containing
+    # sql answer and `${1}_h` containing header fields and ${1}_e for
+    # errors if        .
+    local -n result=$1 result_h=${1}_h result_e=${1}_e
+    result=() result_h=''  result_e=()
+    local line head=""
     shift
-    echo >&$SQLIN "$@" # Send command request, then
 
-    ### Ask for outputing bound... This for sqlite:
-    echo >&$SQLIN "SELECT STRFTIME('%s',DATETIME('now')) AS \"$bound\";"
-    ### This for psql:
-    #  echo >&$SQLIN "SELECT EXTRACT('EPOCH' FROM now()) AS \"$bound\";"
-    ### This for mysql:
-    #  echo >&$SQLIN "SELECT UNIX_TIMESTAMP() AS \"$bound\";"
+    ### Send request and request for outputing bound...
+    printf >&$SQLIN '%s;\nSELECT '"${sqlreqbound}"' AS "%s";\n' "$@" $bound
 
     read -ru $SQLOUT line
     if [ "$line" != "$bound" ] ;then
-	IFS=$'\t' read -a ${result}_h <<< "$line";
+	IFS=$'\t' read -a result_h <<< "$line";
 	while read -ru $SQLOUT line && [ "$line" != "$bound" ] ;do
-	    eval "$result+=(\"\$line\")"
+	    result+=("$line")
 	done
     fi
     read -ru $SQLOUT line
     lastsqlread="$line"
-    # then once bound readed without timeout, we could read SQLERR with
-    # very short timeout
-    while read -ru $SQLERR -t .0002 line;do
-	# eval "${result}_e+=(\"\$line\")"
-	echo >&2 "$line"
-    done
-}
-myPsql() {
-    local result=$1 line head="";shift ; echo >&$SQLIN "$@";
-    echo >&$SQLIN "SELECT EXTRACT('EPOCH' FROM now()) AS \"$bound\";"
-    read -ru $SQLOUT line
-    if [ "$line" != "$bound" ] ;then
-	IFS=$'\t' read -a ${result}_h <<< "$line";
-	while read -ru $SQLOUT line && [ "$line" != "$bound" ] ;do
-	    eval "$result+=(\"\$line\")"
+    # then once bound readed without timeout, we could read SQLERR
+    # read -t 0 don't read, but success only if data available
+    if read -u $SQLERR -t 0 ;then
+	while read -ru $SQLERR -t .02 line;do
+	    result_e+=("$line")
 	done
     fi
-    read -ru $SQLOUT line
-    lastsqlread="$line"
-    while read -ru $SQLERR -t .0002 line;do
-	echo >&2 "$line"
-    done
 }
-myMysql() {
-    local result=$1 line head="" ;shift
-    echo >&$SQLIN "$@"; echo >&$SQLIN "SELECT UNIX_TIMESTAMP() AS \"$bound\";"
-    read -ru $SQLOUT line
-    if [ "$line" != "$bound" ] ;then
-	IFS=$'\t' read -a ${result}_h <<< "$line";
-	while read -ru $SQLOUT line && [ "$line" != "$bound" ] ;do
-	    eval "$result+=(\"\$line\")"
-	done; fi
-    read -ru $SQLOUT line
-    lastsqlread="$line"
-    while read -ru $SQLERR -t .0002 line;do echo >&2 "$line";done
-}
-
-# As each fifo used by connectors are already deleted, just drop
-# fifo's directory on exit.
-trap "rmdir $TMPDIR;exit" 0 1 2 3 6 9 15
+    
+# newSqlConnector /usr/bin/sqlite3 $'-separator \t -header /dev/shm/test.sqlite'
+# newSqlConnector /usr/bin/psql $'-Anh hostname -F \t --pset=footer=off user'
+# newSqlConnector /usr/bin/mysql  '-h hostname -B -p database'
